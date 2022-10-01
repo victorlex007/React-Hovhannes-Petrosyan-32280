31660d73fb64f273a6e76aa702fba86fa29538b0
Victor Lezcano <victormaverik@gmail.com>
2022-09-30 22:17:42 -0300

    finalreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Cartwidget } from './Components/CartWidget/Cartwidget';
+import  Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
+import CartContainer from "./Components/CartContainer/CartContainer";
+import CheckOutContainer from "./Components/CheckOutContainer/CheckOutContainer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        minHeight: "100vh",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    <ItemListContainer />
+    <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/cart" element={<CartContainer />} />
+          <Route path="/checkout" element={<CheckOutContainer />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import { getFirestore } from 'firebase/firestore';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import CartContext from './Components/CartContext/CartContext';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdJOTN-F01D1od7ZLUluNLPZ7BF-6ewcs",
+  authDomain: "e-commercewagyu.firebaseapp.com",
+  projectId: "e-commercewagyu",
+  storageBucket: "e-commercewagyu.appspot.com",
+  messagingSenderId: "484698496161",
+  appId: "1:484698496161:web:a5cd36487b163b0ac764e0",
+  measurementId: "G-Y7QTMW01NC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
+  <CartContext>
+    <ChakraProvider>
     <App />
-  </React.StrictMode>
+    </ChakraProvider>
+    </CartContext>
+  </>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
